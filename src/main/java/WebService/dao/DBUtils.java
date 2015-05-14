0744a36a0b989c574a53c1b9c409ae78b265af56
LamronNu <olga.prylypko@gmail.com>
2015-05-14 10:42:31 +0300

    create tables

--- src/main/java/WebService/dao/DBUtils.java
+++ src/main/java/WebService/dao/DBUtils.java
@@ -3,7 +3,6 @@ package WebService.dao;
 import Library.Consts;
 import org.apache.log4j.Logger;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -21,7 +20,7 @@ public class DBUtils {
     private static final Logger log = Logger.getLogger(DBUtils.class);
     private static Connection connection = null;
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException {
         if (connection != null)
             return connection;
         else {
@@ -48,17 +47,9 @@ public class DBUtils {
                     Class.forName(driver);
                     connection = DriverManager.getConnection(url, user, password);
                 }
-            } catch (ClassNotFoundException e) {
+            } catch (ClassNotFoundException | IOException | URISyntaxException e) {
                 log.error("catch ex:" + e.getMessage());
                 //e.printStackTrace();
-            } catch (SQLException e) {
-                log.error("catch ex:" + e.getMessage());
-            } catch (FileNotFoundException e) {
-                log.error("catch ex:" + e.getMessage());
-            } catch (IOException e) {
-                log.error("catch ex:" + e.getMessage());
-            } catch (URISyntaxException e) {
-                log.error("catch ex:" + e.getMessage());
             }
             return connection;
         }
@@ -76,70 +67,58 @@ public class DBUtils {
     }
 
     public static void createTables() throws SQLException {
-
+        //postgresql
         Statement stmt = getConnection().createStatement();
 
         String sqlCreate = "" +
                 /*Users*/
-                "DROP TABLE \"users\"";
-        log.info("drop users " + sqlCreate);
-        stmt.execute(sqlCreate);
-        sqlCreate = "" +
-                "CREATE TABLE IF NOT EXISTS \"users\"\n" +
-                "(\n" +
-                "    id serial PRIMARY KEY,\n" +
-                "    createdOn  timestamp DEFAULT current_timestamp NOT NULL,\n" +
-                "    Login varchar(50) NOT NULL,\n" +
-                "    password varchar(50) NOT NULL,\n" +
-                "    firstName varchar(50) NOT NULL,\n" +
-                "    lastName varchar(50)\n" +
-                ")";
-        //                "/*ALTER TABLE users ADD CONSTRAINT unique_userid UNIQUE (id);*/\n" +
+                "CREATE TABLE IF NOT EXISTS users\n" +
+                "                (\n" +
+                "                id serial PRIMARY KEY,\n" +
+                "                createdOn  timestamp DEFAULT current_timestamp NOT NULL,\n" +
+                "                login varchar(50) UNIQUE  NOT NULL,\n" +
+                "                password varchar(50) NOT NULL,\n" +
+                "                firstName varchar(50) NOT NULL,\n" +
+                "                lastName varchar(50)\n" +
+                "                );";
 
         log.info("users" + sqlCreate);
         stmt.execute(sqlCreate);
 
         sqlCreate = "" +
                 /*lots*/
-                "CREATE TABLE IF NOT EXISTS \"lots\"\n" +
-                "(\n" +
-                "    id serial PRIMARY KEY,\n" +
-                "    createdOn  timestamp DEFAULT current_timestamp NOT NULL,\n" +
-                "    name varchar(100) NOT NULL,\n" +
-                "    finishDate  timestamp DEFAULT current_timestamp NOT NULL,\n" +
-                "    startPrice float(15,2) NOT NULL,\n" +
-                "    description varchar(500),\n" +
-                "    ownerId int  NOT NULL,\n" +
-                "    state varchar(10) DEFAULT \"Active\" NOT NULL\n" +
-                ")\n";
-
-        log.info("lots" + sqlCreate);
-        stmt.execute(sqlCreate);
-
-        sqlCreate = "" +
-//                "/*ALTER TABLE lots ADD CONSTRAINT unique_lotid UNIQUE (id);*/\n" +
+                "CREATE TABLE IF NOT EXISTS lots\n" +
+                "                (\n" +
+                "                id serial PRIMARY KEY,\n" +
+                "                createdOn  timestamp DEFAULT current_timestamp NOT NULL,\n" +
+                "                name varchar(100) NOT NULL,\n" +
+                "                finishDate  timestamp DEFAULT current_timestamp NOT NULL,\n" +
+                "                startPrice float(2) NOT NULL,\n" +
+                "                description varchar(500),\n" +
+                "                ownerId int  NOT NULL,\n" +
+                "                state varchar(10) DEFAULT 'Active' NOT NULL\n" +
+                "                )\n" +
+                "                ;\n" +
                 "ALTER TABLE lots ADD CONSTRAINT lots_user\n" +
-                "  FOREIGN KEY (ownerId) REFERENCES users (id);\n" +
-                "\n";
+                "                FOREIGN KEY (ownerId) REFERENCES users (id);";
 
-        log.info("lots FK" + sqlCreate);
+        log.info("lots " + sqlCreate);
         stmt.execute(sqlCreate);
 
         sqlCreate = "" +
 //                "/*bids*/\n" +
                 "CREATE TABLE IF NOT EXISTS bids\n" +
-                "(\n" +
-                "    id  serial PRIMARY KEY ,\n" +
-                "    lotId int NOT NULL,\n" +
-                "    ownerId int NOT NULL,\n" +
-                "    value float (15, 2) NOT NULL,\n" +
-                "    createdOn  timestamp DEFAULT current_timestamp  NOT NULL\n" +
-                ");\n" +
-//                "/*ALTER TABLE bids ADD CONSTRAINT unique_bidid UNIQUE (id);*/\n" +
+                "                (\n" +
+                "                id  serial PRIMARY KEY ,\n" +
+                "                lotId int NOT NULL,\n" +
+                "                ownerId int NOT NULL,\n" +
+                "                value float (2) NOT NULL,\n" +
+                "                createdOn  timestamp DEFAULT current_timestamp  NOT NULL\n" +
+                "                );\n" +
                 "ALTER TABLE bids ADD CONSTRAINT bids_user\n" +
-                "  FOREIGN KEY (ownerId) REFERENCES users (id);\n" +
+                "                FOREIGN KEY (ownerId) REFERENCES users (id);\n" +
                 "ALTER TABLE bids ADD CONSTRAINT lots_user\n" +
-                "  FOREIGN KEY (lotId) REFERENCES lots (id)";
+                "                FOREIGN KEY (lotId) REFERENCES lots (id);";
 
         log.info("bids" + sqlCreate);
         stmt.execute(sqlCreate);
